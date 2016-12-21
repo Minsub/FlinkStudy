f11d5b44cbcb7325efcd6abe894b07e72de1a4ae
jiminsub <jiminsub@gmail.com>
2016-12-21 22:26:24 +0900

    add Sample App

---
+++ src/main/scala/flink/sample/SampleApp.scala
@@ -0,0 +1,25 @@
+package flink.sample
+
+import flink.generater.StreamCreator
+import org.apache.flink.streaming.api.TimeCharacteristic
+import org.apache.flink.streaming.api.scala._
+import org.apache.flink.streaming.api.windowing.time.Time
+
+object SampleApp {
+
+  def main(args: Array[String]): Unit = {
+    val env = StreamExecutionEnvironment.getExecutionEnvironment
+    env.setStreamTimeCharacteristic(TimeCharacteristic.ProcessingTime)
+    env.setParallelism(2)
+
+    val input = env.fromCollection[Int](StreamCreator.source[Int](List.range(1, 10), 200))
+
+    input.map((_, 1))
+      .timeWindowAll(Time.seconds(5), Time.seconds(2))
+      .reduce((x, y) => (x._1 + y._1, x._2 + y._2))
+      .print()
+
+    env.execute("Flink Run Test1")
+  }
+
+}
