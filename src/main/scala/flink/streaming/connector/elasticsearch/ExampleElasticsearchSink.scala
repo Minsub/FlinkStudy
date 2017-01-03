3bb2cd723be484a7632ca17ae32916ceeb214c5f
jiminsub <jiminsub@gmail.com>
2017-01-03 21:07:44 +0900

    update elasticsearch samples

--- src/main/scala/flink/streaming/connector/elasticsearch/ExampleElasticsearchSink.scala
+++ src/main/scala/flink/streaming/connector/elasticsearch/ExampleElasticsearchSink.scala
@@ -1,8 +1,5 @@
 package flink.streaming.connector.elasticsearch
 
-//import java.util
-
-//import org.apache.flink.api.common.functions.RuntimeContext
 import java.net.{InetAddress, InetSocketAddress}
 import java.util
 
@@ -11,6 +8,7 @@ import org.apache.flink.api.scala._
 import org.apache.flink.streaming.api.TimeCharacteristic
 import org.apache.flink.streaming.api.scala.StreamExecutionEnvironment
 import org.apache.flink.streaming.connectors.elasticsearch2.{ElasticsearchSink, ElasticsearchSinkFunction, RequestIndexer}
+import org.elasticsearch.action.index.IndexRequest
 import org.elasticsearch.client.Requests
 
 
@@ -20,12 +18,12 @@ object ExampleElasticsearchSink {
     val env = StreamExecutionEnvironment.getExecutionEnvironment
     env.setStreamTimeCharacteristic(TimeCharacteristic.ProcessingTime)
     env.setParallelism(2)
-//
+
     val input = env.fromElements("[START] this is test message [END]")
     val stream  = input.flatMap(_.split(" "))
 
     val transports = new util.ArrayList[InetSocketAddress]
-    transports.add(new InetSocketAddress(InetAddress.getByName("127.0.0.1"),9200))
+    transports.add(new InetSocketAddress(InetAddress.getByName("127.0.0.1"), 9300))
 
 
     val config = new util.HashMap[String, String]
@@ -33,11 +31,14 @@ object ExampleElasticsearchSink {
     config.put("cluster.name", "elasticsearch")
 
     stream.addSink(new ElasticsearchSink[String](config, transports, new ElasticsearchSinkFunction[String] {
-      override def process(element: String, ctx: RuntimeContext, indexer: RequestIndexer): Unit = {
+      def createIndexRequest(element: String): IndexRequest = {
         val json = new util.HashMap[String, AnyRef]
         json.put("data", element)
-        println("SENDING: " + element)
         Requests.indexRequest.index("flink-1").`type`("my-type").source(json)
+      }
+
+      override def process(element: String, ctx: RuntimeContext, indexer: RequestIndexer): Unit = {
+        indexer.add(createIndexRequest(element))
       }
     }))
 
