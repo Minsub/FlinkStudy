7feb8323960fb8df4e8fbf8d403f7c016bc02938
jiminsub <jiminsub@gmail.com>
2017-01-08 17:14:28 +0900

    modify kafka consumer

--- src/main/scala/flink/streaming/connector/kafka/ExampleKafkaComsumer.scala
+++ src/main/scala/flink/streaming/connector/kafka/ExampleKafkaComsumer.scala
@@ -7,6 +7,7 @@ import org.apache.flink.streaming.api.TimeCharacteristic
 import org.apache.flink.streaming.api.scala.StreamExecutionEnvironment
 import org.apache.flink.streaming.connectors.kafka._
 import org.apache.flink.streaming.util.serialization.SimpleStringSchema
+import org.apache.kafka.common.serialization.StringDeserializer
 
 /**
   * Created by hmm1115222 on 2016-12-14.
@@ -15,17 +16,21 @@ object ExampleKafkaComsumer {
 
   def main(args: Array[String]): Unit = {
     val env = StreamExecutionEnvironment.getExecutionEnvironment
+    env.enableCheckpointing(1000) // required for commit offset by consumer group
     env.setStreamTimeCharacteristic(TimeCharacteristic.ProcessingTime)
-    env.setParallelism(2)
+    env.setParallelism(3)
 
     val properties = new Properties();
     properties.setProperty("bootstrap.servers", "localhost:9092");
-    // only required for Kafka 0.8
-    properties.setProperty("zookeeper.connect", "localhost:2181");
-    properties.setProperty("group.id", "Flink Connector");
-    val consumer = new FlinkKafkaConsumer09[String]("test", new SimpleStringSchema(), properties)
-    val stream = env.addSource(consumer)
 
+    // only required for Kafka 0.8
+//    properties.setProperty("zookeeper.connect", "localhost:2181");
+    properties.setProperty("group.id", "consumer-tutorial-group2");
+    properties.put("key.deserializer", classOf[StringDeserializer].getName)
+    properties.put("value.deserializer", classOf[StringDeserializer].getName)
+
+    val consumer = new FlinkKafkaConsumer09[String]("consumer-tutorial", new SimpleStringSchema(), properties)
+    val stream = env.addSource(consumer)
 
     stream.print()
 
