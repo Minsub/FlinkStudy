a5559b625e9d605888f3f815d8813fa3c0e6f5f0
jiminsub <jiminsub@gmail.com>
2017-01-09 23:50:50 +0900

    add flink kafka example

---
+++ src/main/scala/flink/streaming/connector/kafka/ExampleKafka.scala
@@ -0,0 +1,36 @@
+package flink.streaming.connector.kafka
+
+import java.util.Properties
+
+import org.apache.flink.api.scala._
+import org.apache.flink.streaming.api.TimeCharacteristic
+import org.apache.flink.streaming.api.scala.StreamExecutionEnvironment
+import org.apache.flink.streaming.connectors.kafka._
+import org.apache.flink.streaming.util.serialization.SimpleStringSchema
+import org.apache.kafka.common.serialization.StringDeserializer
+
+
+object ExampleKafka {
+
+  def main(args: Array[String]): Unit = {
+    val env = StreamExecutionEnvironment.getExecutionEnvironment
+    env.enableCheckpointing(1000) // required for commit offset by consumer group
+    env.setStreamTimeCharacteristic(TimeCharacteristic.ProcessingTime)
+    env.setParallelism(3)
+
+    val properties = new Properties();
+    properties.setProperty("bootstrap.servers", "localhost:9092");
+    properties.setProperty("group.id", "consumer-tutorial-group");
+    properties.put("key.deserializer", classOf[StringDeserializer].getName)
+    properties.put("value.deserializer", classOf[StringDeserializer].getName)
+
+    val consumer = new FlinkKafkaConsumer09[String]("consumer-tutorial", new SimpleStringSchema(), properties)
+    val input = env.addSource(consumer)
+
+    input
+      .map(_ + " by Flink")
+      .addSink(new FlinkKafkaProducer09[String]("localhost:9092", "consumer-tutorial-flink", new SimpleStringSchema()))
+
+    env.execute("Flink Kafka Consumer")
+  }
+}
