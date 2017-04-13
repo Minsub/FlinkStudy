6e368de462fabb766f1c77da623daa31c9eaa934
jiminsub <jiminsub@gmail.com>
2017-04-13 23:52:39 +0900

    modify batch samples

--- src/main/scala/flink/batch/WordCount.scala
+++ src/main/scala/flink/batch/WordCount.scala
@@ -1,18 +1,26 @@
 package flink.batch
 
 import org.apache.flink.api.scala._
+import org.apache.flink.core.fs.FileSystem.WriteMode
 
 object WordCount {
 
   def main(args: Array[String]): Unit = {
     val env = ExecutionEnvironment.getExecutionEnvironment
+    env.setParallelism(1)
+
+    // read from file
     val text = env.readTextFile("src/main/resources/README_FLINK.md")
     val count = text
       .flatMap(_.split(" "))
       .map((_, 1))
       .groupBy(0)
       .sum(1)
+      .filter(_._2 > 10)
 
-    count.print()
+    // write to file
+    count.writeAsText("src/main/resources/wordcount.txt", WriteMode.OVERWRITE);
+
+    env.execute("flink batch example")
   }
 }
